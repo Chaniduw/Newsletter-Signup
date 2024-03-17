35b8146e47b72af7b9f07c6a802d42240c9400a9
Chaniduw <chanidunuwanajith@gmail.com>
2024-03-18 00:31:24 +0530

    post data to the  mailchimp

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",function (req,res){
+app.post("/", function (req, res) {
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
 
-    
-})
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/6f5b1e98c2";
+
+  const options = {
+    method: "POST",
+    auth: "chanidu:0a5629c24bc01a2aa9cc9ac0acc0c534-us8",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, function () {
   console.log("Server is running in port 3000");
 });
+
+//
+//6f5b1e98c2
